e8a6afb564b75f30e1259107d85f7230eda12281
sergiifartushnyi <sergiyfartyshnyy@gmail.com>
2025-02-20 22:25:48 +0200

    hw22.1

--- cashe.py
+++ cashe.py
@@ -1,17 +1,56 @@
-from functools import lru_cache
+import redis
+import datetime
 
-@lru_cache(maxsize=None)
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+call_count = 0
+
+t1 = "dataline"
+
 def fibonacci(n):
-    if n <= 2:
-        return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    global call_count
 
-def get_numbers():
+    cached_value = r.get(f"fibonacci:{n}")
+    if cached_value:
+        call_count += 1
+        return int(cached_value)
 
-    result = [fibonacci(18), fibonacci(19), fibonacci(20), 9999]
+    if n <= 2 :
+        result = 1
+    else:
+        a, b = 1, 1
+        for i in range(3, n + 1):
+            a, b = b, a + b
+        result = b
+
+    r.set(f"fibonacci:{n}", result)
     return result
 
+def get_numbers():
+    result = []
+    for i in range(1, 10000):
+        result.append(fibonacci(i))
+    return result
+
+def print_number_in_column(number):
+    number_str = str(number)
+
+
+    for i in range(0, len(number_str), 4):
+        print(number_str[i:i+4])
+
 if __name__ == "__main__":
-    numbers = get_numbers()
-    for num in numbers:
-        print(num)
+    start_time = datetime.datetime.now()
+
+    try:
+        numbers = get_numbers()
+        for num in numbers:
+            print_number_in_column(num)
+    except redis.RedisError as e:
+        print(f"Error with Redis: {e}")
+
+    end_time = datetime.datetime.now()
+    elapsed_time = end_time - start_time
+
+    print(f"Fibonacci function called {call_count} times.")
+    print(f"Execution time: {elapsed_time}")
